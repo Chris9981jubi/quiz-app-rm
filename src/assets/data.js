48d434b8ab6e5255b884110a96e83f77b80dcf7f
Christy <christypjubi@gmail.com>
2024-07-18 19:37:28 +0530

    website completed

---
+++ src/assets/data.js
@@ -0,0 +1,43 @@
+export const data = [
+    {
+        question : "What is the primary purpose of the 'super' keyword in Java?",
+        option1: "To call methods of the parent class ",
+        option2: "To declare static methods",
+        option3: "To override methods in the subclass",
+        option4: "To increase the visibility of method",
+        ans:1
+    },
+    {
+        question : "During program development, software requirements specify",
+        option1: "How the program will accomplish the task",
+        option2: "What the task is that the program must perform",
+        option3: "How to divide the task into subtasks",
+        option4: "How to test the program when it is done",
+        ans:2
+    },
+    {
+        question:"_______ is the process of finding errors and fixing them within a program.",
+        option1 : "Compiling",
+        option2 : "Executing",
+        option3 :" Debugging",
+        option4: "scanning",
+        ans:3
+    },
+    {
+           question: "Sal needs to execute a section of code ten times within a program. Compare the selection structures below and select which one meets the needs identified.",
+           option1: "If-else",
+           option2:"For",
+           option3:"While",
+           option4:"if",
+           ans:2
+    },
+    {
+        question : "A loop that never ends is referred to as a(n)_________",
+        option1:"While loop",
+        option2:"infinite loop",
+        option3:"Recursive loop",
+        option4: "for loop",
+        ans:2,
+    }
+
+]
